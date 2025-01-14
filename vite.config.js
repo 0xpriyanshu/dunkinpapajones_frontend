4549afd8d7e34bd586784e23a973025a60ecfd7f
Priyanshu Kumar <98304940+0xpriyanshu@users.noreply.github.com>
2025-01-14 12:06:48 +0530

    base change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dunkin_papajones_frontend/',
+  base: '/dunkinpapajones_frontend/',
 })
